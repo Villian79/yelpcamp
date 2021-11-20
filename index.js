b64bdd27043bbbb915fdc303c39c23d288ff822e
Villian79 <ivilinchuk@gmail.com>
2021-11-20 20:29:06 +0200

    EJS template engine setup. Views folder added

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const port = 3000;
 
+//EJS setup
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//Test route
 app.get('/', (req, res) => {
-    res.send('Server has been started and the App is running');
+    res.render('index', { name: 'Brother!' });
 });
 
+
+//Start server
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 });
