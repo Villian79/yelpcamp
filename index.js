8f8660d66705b71052fc16238f85a683af35a9ce
Villian79 <ivilinchuk@gmail.com>
2021-12-03 22:30:35 +0200

    Add templates engine ejs-mate

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 const Campground = require('./models/campground');
 
@@ -17,6 +18,7 @@ db.on('error', console.error.bind(console, 'Connection error:'));
 db.once('open', () => console.log('MongoDB has been connected'));
 
 //EJS setup
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
